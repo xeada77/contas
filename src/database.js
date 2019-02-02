cb3690f541b6b57ed833ed4414c0135bf52604b4
Luis Martinez <luismartinez@MacBook-Pro-de-Luis.local>
2019-02-02 17:05:52 +0100

    comentada opcion useNewUrlParser

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/contas-app', {
     useCreateIndex: true,
-    useNewUrlParser: true,
+    //useNewUrlParser: true,
     useFindAndModify: false
 }).then(() => {
     console.log('Base de datos conectada');
